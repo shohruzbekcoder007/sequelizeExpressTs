d606f7facd913d1ed5536d81db82faddae13b326
shohruzbekcoder007 <shohruzbekcoder007@gmail.com>
2024-03-03 16:38:28 +0300

    personal change

--- src/startup/db/index.ts
+++ src/startup/db/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
+import path from "path";
 import { Sequelize } from "sequelize";
+import winston from "winston";
 
 dotenv.config()
 
@@ -9,10 +11,22 @@ const db_postgresql: string = process.env.db_postgresql || ""
 const db_port: string | 5432 = process.env.db_port || 5432
 const host: string = process.env.host || "localhost"
 
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  transports: [
+    new winston.transports.File({ filename: path.join('logs', 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join('logs', 'info.log'), level: 'info' }),
+    new winston.transports.File({ filename: path.join('logs', 'combined.log') }),
+  ],
+});
+
 export const sequelize = new Sequelize(db_postgresql, db_user, db_pass, {
     host: host,
     dialect: 'postgres',
     port: <number>db_port,
+    logging: (msg) => logger.info(msg),
 });
 
 sequelize.sync({ force: false })
